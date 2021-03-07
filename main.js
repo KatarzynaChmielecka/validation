53619f0191c6721d39a2554410722ccee93150d4
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2021-03-07 21:37:28 +0100

    styled index site, added red color to errors, added wrappers

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict"
 
-const h3 = document.querySelector('.contact-form__title');
 const submit = document.querySelector('.contact-form__submit')
 
 let firstName = document.querySelector('.contact-form__name');
@@ -24,42 +23,50 @@ const regMail = /\w+@+[a-z]+.[a-z]{2,}/;
 const regTel = /^\d{9}$/;
 const regWww = /^http:\/\/+\w+.\D{2,}/;
 
-const sendMessage = () => {
-	if (firstName.value.length < 3) {
-		firstNameError.innerText = "Imię musi składać się z minimum 3 znaków"
-	} else {
-		firstNameError.innerText = '';
-	}
 
-	if (surname.value.length < 3) {
-		surnameError.innerText = "Nazwisko musi składać się z minimum 3 znaków";
-	} else {
-		surnameError.innerText = '';
-	}
 
-	if (!regMail.test(mail.value)) {
-		mailError.innerText = "Wzór poprawnego maila: as2@gl.co"
-	} else {
-		mailError.innerText = '';
-	}
+const errors = () => {
+    if (firstName.value.length < 3) {
+        firstNameError.innerText = "Imię musi składać się z minimum 3 znaków";
+        firstNameError.style.color = "#d11507";
+    } else {
+        firstNameError.innerText = '';
+    }
 
-	if (!regTel.test(phone.value)) {
-		phoneError.innerText = "Podaj 9-cyfrową liczbę dodatnią"
-	} else {
-		phoneError.innerText = '';
-	}
+    if (surname.value.length < 3) {
+        surnameError.innerText = "Nazwisko musi składać się z minimum 3 znaków";
+        surnameError.style.color = "#d11507";
+    } else {
+        surnameError.innerText = '';
+    }
 
-	if (!regWww.test(www.value)) {
-		wwwError.innerText = "Wzór poprawnego adresu url: http://blog.pl"
-	} else {
-		wwwError.innerText = '';
-	}
+    if (!regMail.test(mail.value)) {
+        mailError.innerText = "Wzór poprawnego maila: as2@gl.co";
+        mailError.style.color = "#d11507";
+    } else {
+        mailError.innerText = '';
+    }
 
-	if (problem.value.length < 50) {
-		problemError.innerText = "Wiadomość musi składać się z minimum 50 znaków"
-	} else {
-		problemError.innerText = '';
-	}
+    if (!regTel.test(phone.value)) {
+        phoneError.innerText = "Podaj 9-cyfrową liczbę dodatnią";
+        phoneError.style.color = "#d11507";
+    } else {
+        phoneError.innerText = '';
+    }
+
+    if (!regWww.test(www.value)) {
+        wwwError.innerText = "Wzór poprawnego adresu url: http://blog.pl";
+        wwwError.style.color = "#d11507";
+    } else {
+        wwwError.innerText = '';
+    }
+
+    if (problem.value.length < 50) {
+        problemError.innerText = "Wiadomość musi składać się z minimum 50 znaków";
+        problemError.style.color = "#d11507";
+    } else {
+        problemError.innerText = '';
+    }
 }
 
 let a = Math.floor(Math.random() * 10 + 1);
@@ -67,74 +74,74 @@ let b = Math.floor(Math.random() * 10 + 1);
 let answer;
 
 let randomNumber = () => {
-	let mathOperation = Math.floor(Math.random() * 4 + 1);
+    let mathOperation = Math.floor(Math.random() * 4 + 1);
 
 
-	if (mathOperation === 1) {
-		console.log('Podaj wynik dodawania')
-		console.log(a)
-		console.log(b)
-		console.log(a + b)
+    if (mathOperation === 1) {
+        console.log('Podaj wynik dodawania')
+        console.log(a)
+        console.log(b)
+        console.log(a + b)
 
-		answer = a + b;
-		captcha.innerText = `${a} + ${b}`;
+        answer = a + b;
+        captcha.innerText = `${a} + ${b}`;
 
-	} else if (mathOperation === 2) {
-		console.log('Podaj wynik odejmowania')
-		console.log(a)
-		console.log(b)
-		console.log(a - b);
+    } else if (mathOperation === 2) {
+        console.log('Podaj wynik odejmowania')
+        console.log(a)
+        console.log(b)
+        console.log(a - b);
 
-		answer = a - b;
-		captcha.innerText = `${a} - ${b}`;
-	} else if (mathOperation === 3) {
-		console.log('Podaj wynik mnożenia');
-		console.log(a)
-		console.log(b)
-		console.log(a * b)
+        answer = a - b;
+        captcha.innerText = `${a} - ${b}`;
+    } else if (mathOperation === 3) {
+        console.log('Podaj wynik mnożenia');
+        console.log(a)
+        console.log(b)
+        console.log(a * b)
 
-		answer = a * b;
-		captcha.innerText = `${a} * ${b}`;
-	} else if (mathOperation === 4) {
-		console.log('Podaj wynik dzielenia');
-		console.log(a)
-		console.log(2)
-		console.log(a / 2)
+        answer = a * b;
+        captcha.innerText = `${a} * ${b}`;
+    } else if (mathOperation === 4) {
+        console.log('Podaj wynik dzielenia');
+        console.log(a)
+        console.log(2)
+        console.log(a / 2)
 
-		answer = a / 2;
-		captcha.innerText = `${a} / 2`
-	}
-	else {
-		console.log('Coś poszło nie tak, odśwież stronę');
-	}
+        answer = a / 2;
+        captcha.innerText = `${a} / 2`
+    }
+    else {
+        console.log('Coś poszło nie tak, odśwież stronę');
+    }
 }
 
 
 randomNumber();
 
 const saveSummary = () => {
-	localStorage.setItem('firstName', firstName.value);
+    localStorage.setItem('firstName', firstName.value);
+    localStorage.setItem('surname', surname.value);
+    localStorage.setItem('mail', mail.value);
+    localStorage.setItem('phone', phone.value);
+    localStorage.setItem('www', www.value);
+    localStorage.setItem('problem', problem.value);
 }
 
-let captchaFunction = (e) => {
-	sendMessage();
-	saveSummary();
-	if (captchaAnswer.value == answer && firstName.value.length >= 3 && surname.value.length >= 3
-		&& regMail.test(mail.value) && regTel.test(phone.value) && regWww.test(www.value) && problem.value.length >= 50
-	) {
-		console.log('brawo, dobry wynik')
-		window.open('summary.html', 'target="_blank"');
+let sendMessage = (e) => {
+    errors();
+    saveSummary();
 
-	} else {
-		console.log('podaj poprawną odpowiedź lub/i wypełnij poprawnie formularz');
-
-		e.preventDefault()
-
-	}
-
+    if (captchaAnswer.value == answer && firstName.value.length >= 3 && surname.value.length >= 3
+        && regMail.test(mail.value) && regTel.test(phone.value) && regWww.test(www.value) && problem.value.length >= 50
+    ) {
+        console.log('brawo, dobry wynik')
+        window.open('summary.html', 'target="_blank"');
+    } else {
+        e.preventDefault();
+        captchaError.textContent = 'Podaj poprawną odpowiedź lub/i wypełnij poprawnie formularz';
+        captchaError.style.color = "#d11507";
+    }
 }
 
-submit.addEventListener('click', captchaFunction);
-
-
-
+submit.addEventListener('click', sendMessage);
--- summary.js
+++ summary.js
@@ -7,6 +7,11 @@ const summaryFunction = () => {
 	const summaryProblem = document.querySelector('.summary__problem');
 
 	summaryName.innerHTML = localStorage.getItem('firstName');
+	summarySurname.innerHTML = localStorage.getItem('surname');
+	summaryMail.innerHTML = localStorage.getItem('mail');
+	summaryPhone.innerHTML = localStorage.getItem('phone');
+	summaryWWW.innerHTML = localStorage.getItem('www');
+	summaryProblem.innerHTML = localStorage.getItem('problem');
 }
 
 window.addEventListener('load', summaryFunction);
